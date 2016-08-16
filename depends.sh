f36bb9abcbb662d9922297e4e8d451edeb03625a
zhangzheng <zhangzheng@qianbao.com>
2016-08-16 04:05:15 +0000

    增加depends.sh

---
+++ depends.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+yum -y install python34
+yum -y install mariadb mariadb-server mariadb-devel
+yum -y install python34-devel.x86_64
+
+# curl https://bootstrap.pypa.io/get-pip.py | python3.4
+python3.4 utils/get-pip.py
+pip3 install Django==1.9.5
+pip3 install mysqlclient
+pip3 install djangorestframework
+pip3 install markdown
+pip3 install django-filter
