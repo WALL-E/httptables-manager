9e0b839e73d7fdfd276a0cf9286b2f714487b7c6
zhangzheng <zhangzheng@qianbao.com>
2016-08-24 08:18:44 +0000

    部署django-suit模板

--- manager/project/settings.py
+++ manager/project/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+from django.conf.global_settings import TEMPLATE_CONTEXT_PROCESSORS as TCP
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -31,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'suit',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -163,3 +166,6 @@ LOGGING = {
     },
 }
 
+TEMPLATE_CONTEXT_PROCESSORS = (TCP,
+    'django.core.context_processors.request',
+)
